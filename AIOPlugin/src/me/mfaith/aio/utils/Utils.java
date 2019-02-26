c126011cec2a4c2b4e9279b6c8d5223e72873465
MFaith <40504666+MihailoRistic@users.noreply.github.com>
2019-02-26 00:19:22 -0800

    Update Utils.java
    experimental shortening

--- AIOPlugin/src/me/mfaith/aio/utils/Utils.java
+++ AIOPlugin/src/me/mfaith/aio/utils/Utils.java
@@ -6,8 +6,20 @@ import org.bukkit.ChatColor;
 
 public class Utils {
 
-	public static String chat(String s) {
-		return ChatColor.translateAlternateColorCodes('&',s);
+	public static String chat(String s, String ...args) {
+		int i=s.indexOf("<{",0);
+		int x=0;
+		while (i!=-1 && x<args.length){
+			j=s.indexOf("}>",i);
+			s.replace(s.subSequence(i,j), args[x]);
+			x++;
+			i=s.indexOf("<{",i);
+		}
+		if (x=args.length){
+			return ChatColor.translateAlternateColorCodes('&',s);
+		} else {
+			return (ChatColor.translateAlternateColorCodes('&',s) + "Invalid argument amount, inputed "+args.length+", needed x");
+		}
 	}
 	
 	public static String line(String ...s) {
